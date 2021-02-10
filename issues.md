31349137f3f279db79d3dab31677b7cdc2fff815
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-02-09 22:26:38 -0500

    started work on JS

--- issues.md
+++ issues.md
@@ -1,3 +1,3 @@
-root error issue
+root error issue--issue is with whatever is the first item listed
 
 reduce CSS redundancy on form 
---
+++ main.js
@@ -0,0 +1,13 @@
+//on submit, change field color to red and with special message
+//toggle css
+//input text and graphic into html
+//email use RegEX-344 JS uddemy
+
+const submitButton = document.getElementById("submit")
+
+// function checkInputs = 
+//     if firstname = !Textcheck ()
+//     toggle (error class)
+//     else ('')
+
+submitButton.addEventListener('click', console.log('hi there'))
