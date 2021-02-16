4bd2eefd2feaf02e2c597105712f37f1bf62fde5
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-02-15 22:16:07 -0500

    JS now can put red box around empty field

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const submitButton = document.querySelector('.submit');
 const firstName = document.querySelector('.fn-label');
-const form = document.querySelector('.form');
-
 
 
 submitButton.addEventListener('click', e => {
-    if (firstName === '') {
-        firstName.ClassList.add('error')
-        document.querySelector
+    if (firstName.value === '') {
+        firstName.classList.add('error'); //adds red box to firstname input area
+        document.querySelector('.fn-label-error').classList.add('error'); //add the image and text
+        e.preventDefault(); 
+    } else {
+        firstName.classList.remove('error')
+        document.querySelector('.fn-label-error').classList.remove('error');
     }
 });
+
