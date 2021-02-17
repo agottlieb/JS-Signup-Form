f957a042619c55d2ef0e0eea01e56d63ea6bca25
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-02-16 22:11:45 -0500

    added regex for password JS

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const submitButton = document.querySelector('.submit');
 const firstName = document.querySelector('.fn-label');
 const lastName = document.querySelector('.ln-label');
+const emailAddress = document.querySelector('.ea-label');
+const regexPattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 const password = document.querySelector('.pw-label');
 
 submitButton.addEventListener('click', e => {
@@ -19,6 +21,16 @@ submitButton.addEventListener('click', e => {
         lastName.classList.remove('error')
         document.querySelector('.ln-label-error').classList.remove('error');
     }
+
+
+    if (!emailAddress.value.match(regexPattern)) {
+        emailAddress.classList.add('error');
+        document.querySelector('.ea-label-error').classList.add('error');
+    } else {
+        emailAddress.classList.remove('error')
+        document.querySelector('.ea-label-error').classList.remove('error');
+    }
+
     if (password.value === '') {
         password.classList.add('error'); 
         document.querySelector('.password-error').classList.add('error'); 
