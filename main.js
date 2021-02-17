a532568d5e81d53b6b80680e188dd7e9a6bcf933
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-02-16 21:57:51 -0500

    added JS for last name and password field

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const submitButton = document.querySelector('.submit');
 const firstName = document.querySelector('.fn-label');
-
+const lastName = document.querySelector('.ln-label');
+const password = document.querySelector('.pw-label');
 
 submitButton.addEventListener('click', e => {
     if (firstName.value === '') {
@@ -10,6 +11,21 @@ submitButton.addEventListener('click', e => {
     } else {
         firstName.classList.remove('error')
         document.querySelector('.fn-label-error').classList.remove('error');
+    }
+    if (lastName.value==='') {
+        lastName.classList.add('error');
+        document.querySelector('.ln-label-error').classList.add('error');
+    } else {
+        lastName.classList.remove('error')
+        document.querySelector('.ln-label-error').classList.remove('error');
+    }
+    if (password.value === '') {
+        password.classList.add('error'); 
+        document.querySelector('.password-error').classList.add('error'); 
+        e.preventDefault(); 
+    } else {
+        password.classList.remove('error')
+        document.querySelector('.password-error').classList.remove('error');
     }
 });
 
