28414687c1705d62e718d0c3e540487bc4c1eaaf
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-03-01 12:33:55 -0500

    fixed margin issue on submit button

--- issues.md
+++ issues.md
@@ -3,5 +3,5 @@
 
 5. remove purple line when selected
 
-6. legal issues/form space situation
+
 
--- test.js
+++
@@ -1,8 +0,0 @@
-// testing function to make sure button is connected to the right place
-const form = document.querySelector('.form');
-
-function buttonHandler (){
-    alert ('Hello Again')
-} 
-
-form.addEventListener('click', buttonHandler);
