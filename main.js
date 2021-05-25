c5b43447c3a3a0a5ce341ad9bac3f2e741406af0
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-05-25 11:26:08 -0400

    verify function

--- main.js
+++ main.js
@@ -6,42 +6,8 @@ const emailAddress = document.querySelector('.ea-label');
 const regexPattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 const password = document.querySelector('.pw-label');
 
-form.addEventListener('input', e => {
-    if (firstName.value === '') {
-        firstName.classList.add('error'); //adds red box to firstname input area
-        document.querySelector('.fn-label-error').classList.add('error'); //add the image and text
-        e.preventDefault(); 
-    } else {
-        firstName.classList.remove('error')
-        document.querySelector('.fn-label-error').classList.remove('error');
-    }
-    if (lastName.value==='') {
-        lastName.classList.add('error');
-        document.querySelector('.ln-label-error').classList.add('error');
-        e.preventDefault(); 
-    } else {
-        lastName.classList.remove('error')
-        document.querySelector('.ln-label-error').classList.remove('error');
-    }
 
-    if (!emailAddress.value.match(regexPattern)) {
-        emailAddress.classList.add('error');
-        document.querySelector('.ea-label-error').classList.add('error');
-        e.preventDefault();  
-    } else {
-        emailAddress.classList.remove('error')
-        document.querySelector('.ea-label-error').classList.remove('error');
-    }
 
-    if (password.value === '') {
-        password.classList.add('error'); 
-        document.querySelector('.password-error').classList.add('error'); 
-        e.preventDefault(); 
-    } else {
-        password.classList.remove('error')
-        document.querySelector('.password-error').classList.remove('error');
-    }
-});
 
 submitButton.addEventListener('click', e => {
     if (firstName.value === '') {
@@ -81,4 +47,51 @@ submitButton.addEventListener('click', e => {
 });
 
 
+form.addEventListener('input', e => {
+    if (firstName.value === '') {
+        firstName.classList.add('error'); //adds red box to firstname input area
+        document.querySelector('.fn-label-error').classList.add('error'); //add the image and text
+        e.preventDefault(); 
+    } else {
+        firstName.classList.remove('error')
+        document.querySelector('.fn-label-error').classList.remove('error');
+    }
+    if (lastName.value==='') {
+        lastName.classList.add('error');
+        document.querySelector('.ln-label-error').classList.add('error');
+        e.preventDefault(); 
+    } else {
+        lastName.classList.remove('error')
+        document.querySelector('.ln-label-error').classList.remove('error');
+    }
 
+    if (!emailAddress.value.match(regexPattern)) {
+        emailAddress.classList.add('error');
+        document.querySelector('.ea-label-error').classList.add('error');
+        e.preventDefault();  
+    } else {
+        emailAddress.classList.remove('error')
+        document.querySelector('.ea-label-error').classList.remove('error');
+    }
+
+    if (password.value === '') {
+        password.classList.add('error'); 
+        document.querySelector('.password-error').classList.add('error'); 
+        e.preventDefault(); 
+    } else {
+        password.classList.remove('error')
+        document.querySelector('.password-error').classList.remove('error');
+    }
+});
+
+function verify() {
+    console.log(1);
+} 
+
+submitButton.addEventListener('click', verify )
+
+function check () {
+    if (log > 0) {
+        
+    }
+}
