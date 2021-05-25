99792227d5a92619271234a6c00490e14e5807ef
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-05-25 19:43:44 -0400

    attempt to fix verification issue

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ const lastName = document.querySelector('.ln-label');
 const emailAddress = document.querySelector('.ea-label');
 const regexPattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 const password = document.querySelector('.pw-label');
+const alreadyClicked = false;
 
-
-
-
+//submit button checks for errors
 submitButton.addEventListener('click', e => {
     if (firstName.value === '') {
         firstName.classList.add('error'); //adds red box to firstname input area
@@ -46,52 +45,48 @@ submitButton.addEventListener('click', e => {
     }
 });
 
+//
+function clicked () {
+    alreadyClicked === true
+}
 
-form.addEventListener('input', e => {
-    if (firstName.value === '') {
-        firstName.classList.add('error'); //adds red box to firstname input area
-        document.querySelector('.fn-label-error').classList.add('error'); //add the image and text
-        e.preventDefault(); 
-    } else {
-        firstName.classList.remove('error')
-        document.querySelector('.fn-label-error').classList.remove('error');
-    }
-    if (lastName.value==='') {
-        lastName.classList.add('error');
-        document.querySelector('.ln-label-error').classList.add('error');
-        e.preventDefault(); 
-    } else {
-        lastName.classList.remove('error')
-        document.querySelector('.ln-label-error').classList.remove('error');
-    }
+submitButton.addEventListener('click', clicked )
 
-    if (!emailAddress.value.match(regexPattern)) {
-        emailAddress.classList.add('error');
-        document.querySelector('.ea-label-error').classList.add('error');
-        e.preventDefault();  
-    } else {
-        emailAddress.classList.remove('error')
-        document.querySelector('.ea-label-error').classList.remove('error');
-    }
-
-    if (password.value === '') {
-        password.classList.add('error'); 
-        document.querySelector('.password-error').classList.add('error'); 
-        e.preventDefault(); 
-    } else {
-        password.classList.remove('error')
-        document.querySelector('.password-error').classList.remove('error');
-    }
-});
-
-function verify() {
-    console.log(1);
-} 
-
-submitButton.addEventListener('click', verify )
-
-function check () {
-    if (log > 0) {
-        
-    }
+if (alreadyClicked === true) {
+    form.addEventListener('input', e => {
+        if (firstName.value === '') {
+            firstName.classList.add('error'); //adds red box to firstname input area
+            document.querySelector('.fn-label-error').classList.add('error'); //add the image and text
+            e.preventDefault(); 
+        } else {
+            firstName.classList.remove('error')
+            document.querySelector('.fn-label-error').classList.remove('error');
+        }
+        if (lastName.value==='') {
+            lastName.classList.add('error');
+            document.querySelector('.ln-label-error').classList.add('error');
+            e.preventDefault(); 
+        } else {
+            lastName.classList.remove('error')
+            document.querySelector('.ln-label-error').classList.remove('error');
+        }
+    
+        if (!emailAddress.value.match(regexPattern)) {
+            emailAddress.classList.add('error');
+            document.querySelector('.ea-label-error').classList.add('error');
+            e.preventDefault();  
+        } else {
+            emailAddress.classList.remove('error')
+            document.querySelector('.ea-label-error').classList.remove('error');
+        }
+    
+        if (password.value === '') {
+            password.classList.add('error'); 
+            document.querySelector('.password-error').classList.add('error'); 
+            e.preventDefault(); 
+        } else {
+            password.classList.remove('error')
+            document.querySelector('.password-error').classList.remove('error');
+        }
+    });
 }
